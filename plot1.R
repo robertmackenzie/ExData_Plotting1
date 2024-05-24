334b3b48edf3301744289e5a6a1949b74207d8a4
Robert Mackenzie <robertmackenzie@gmail.com>
2024-05-24 13:43:54 +0100

    Plot 2

--- plot1.R
+++ plot1.R
@@ -1,40 +1,10 @@
-library(readr)
-library(dplyr)
-library(lubridate)
-
 # Effectively disable scientific notation
 options(scipen = 999)
 
-zip_file_name <- "household_power_consumption.zip"
-file_name <- "household_power_consumption.txt"
-status <- download.file(
-  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-  destfile = zip_file_name
-)
-if (status != 0) {
-  stop(sprintf("Cannot download file %s", zip_file_name))
-}
-unzip(zip_file_name, files = file_name)
+# Load the data
+source("./load_data.R")
 
-consumption <- read_delim(
-  file_name,
-  delim = ";",
-  na = c("?", ""),
-  col_types = cols(
-    Date = col_date(format = "%d/%m/%Y"),
-    Time = col_time(format = ""),
-    Global_active_power = col_double(),
-    Global_reactive_power = col_double(),
-    Voltage = col_double(),
-    Global_intensity = col_double(),
-    Sub_metering_1 = col_double(),
-    Sub_metering_2 = col_double(),
-    Sub_metering_3 = col_double()
-  )
-) %>% filter(between(Date, ymd("2007-02-01"), ymd("2007-02-02")))
-
-stop_for_problems(consumption)
-
+# Create the plot
 png(
   filename = "plot1.png",
   width = 480,
---
+++ plot2.R
@@ -0,0 +1,21 @@
+# Effectively disable scientific notation
+options(scipen = 999)
+
+# Load the data
+source("./load_data.R")
+
+# Create the plot
+png(
+  filename = "plot2.png",
+  width = 480,
+  height = 480,
+  units = "px",
+  pointsize = 12
+)
+with(consumption, plot(
+                       ymd_hms(paste(consumption$Date, consumption$Time)),
+                       Global_active_power,
+                       type = "l",
+                       ylab = "Global Active Power (kilowatts)",
+                       xlab = ""))
+dev.off()
